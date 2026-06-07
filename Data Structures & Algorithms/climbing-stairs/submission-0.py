d3799a72bd24401c63655958d3e28b48c0f8763a
David Anukam <109883788+davidanukam@users.noreply.github.com>
2026-06-07 04:03:06 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def countWays(n):
+            prev1 = 1
+            prev2 = 1
+
+            for i in range(2, n + 1):
+                curr = prev1 + prev2
+                prev2 = prev1
+                prev1 = curr
+            
+            return prev1
+        return countWays(n)
